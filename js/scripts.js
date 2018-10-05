23bd4b86b1e3b6963f2c6347999ee84237fc2511
Christabel Escarez <christabel.escarez@gmail.com>
2018-10-04 21:11:18 -0700

    add for loop to replace ping, pong, pingpong into array, clear output after each submit/before displaying next output

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,9 @@ inputArray = [];
 $(document).ready(function(){
   $('form').submit(function(event){
     event.preventDefault();
-    // $("#output").removeClass;
-    // $("#number-input").removeClass;
+    inputArray.length = 0;
+    $("#output").removeClass;
+    $("#number-input").removeClass;
     var inputNum = parseInt($("input").val());
 
     if (inputNum <= 0 || $("input").val() ==="") {
@@ -15,7 +16,13 @@ $(document).ready(function(){
         inputArray.push([i]);
       }
       for (i = 1; i <= inputNum; i++) {
-        if (inputArray[i] % )
+        if (inputArray[i] % 3 === 0) {
+          inputArray[i] = "ping";
+        } else if (inputArray[i] % 5 === 0) {
+          inputArray[i] = "pong";
+        } else if (inputArray[i] % 15 === 0) {
+          inputArray[i] = "ping-pong";
+        }
       }
       $("#output").text("Your sequence is as follows: " + inputArray.join());
     }
